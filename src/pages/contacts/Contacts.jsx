7ebadbc3a8a4cda5e9d86636a999610fbee2626b
Alex Berezov <53948974+Alex-Berezov@users.noreply.github.com>
2021-02-27 10:19:10 +0400

    Made Contacts page

--- src/pages/contacts/Contacts.jsx
+++ src/pages/contacts/Contacts.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
+
 import './contacts.css'
+import whatsapp from '../../assets/img/whatsapp.svg'
+import telegram from '../../assets/img/telegram.svg'
+import skype from '../../assets/img/skype.svg'
+import gmail from '../../assets/img/gmail.svg'
 
 function Contacts() {
     return (
         <div className="contacts">
             <div className="contacts__title block__title">
-                <h2>Портфолио</h2>
+                <h2>Контакты</h2>
                 <span/>
             </div>
             <div className="contacts__contactsForm">
@@ -28,20 +33,24 @@ function Contacts() {
                             <label htmlFor="contact_form_message">Укажите тему сообщения*</label>
                             <textarea name="message" id="contact_form_message" cols="30" rows="6"></textarea>
                         </div>
-                        <button className="button" type="submit">Отправить сообщение</button>
+                        <button className="button" type="submit">Отправить</button>
                     </form>
                 </div>
                 <div className="connection">
                     <div className="connection_infoblock">
                         <div className="infoblock_title">Мессенджеры</div>
                         <div className="infoblock_item">
-
+                            <span><img src={whatsapp} alt="whatsappIcon"/> +374 41-93-93-41</span>
+                            <span><img src={telegram} alt="telegram"/>@Aleber613</span>
+                            <span><img src={skype} alt="skype"/>alex.berezov</span>
                         </div>
-                        <div className="infoblock_item"></div>
-                        <div className="infoblock_item"></div>
                     </div>
-                    <div className="connection_infoblock"></div>
-                    <div className="connection_infoblock"></div>
+                    <div className="connection_infoblock">
+                        <div className="infoblock_title">Email</div>
+                        <div className="infoblock_item">
+                            <span><img src={gmail} alt="gmail"/>aleber197@gmail.com</span>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
