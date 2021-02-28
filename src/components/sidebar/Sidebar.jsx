bf9371120fd36b55bb70c0c5dbdb1c880ed8a4ce
Alex Berezov <53948974+Alex-Berezov@users.noreply.github.com>
2021-02-28 18:34:59 +0400

    Added mobile menu

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,38 +1,52 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {NavLink} from "react-router-dom";
 
 import './sidebar.css'
 import iAm from '../../assets/img/iam.png'
 
 function Sidebar() {
+    const [visibleSidebar, setVisibleSideBar] = useState(false)
+
+    const toggleVisibleSidebar = () => {
+        setVisibleSideBar(!visibleSidebar)
+    }
+
     return (
-        <div className="sidebar">
-            <div className="sidebar__photo">
-                <img src={iAm} alt="Alex Berezov foto" />
+        <>
+            <div onClick={toggleVisibleSidebar} className="sidebar__burger">
+                <span></span>
+                <span></span>
+                <span></span>
             </div>
-            <div className="sidebar__navMenu">
-                <ul>
-                    <li className="active">
-                        <NavLink to="/">Главная</NavLink>
-                    </li>
-                    <li>
-                        <NavLink to="/about">Обо мне</NavLink>
-                    </li>
-                    <li>
-                        <NavLink to="/resume">Резюме</NavLink>
-                    </li>
-                    <li>
-                        <NavLink to="/portfolio">Портфолио</NavLink>
-                    </li>
-                    <li>
-                        <NavLink to="/contacts">Контакты</NavLink>
-                    </li>
-                </ul>
+            <div className={visibleSidebar && visibleSidebar ? 'sidebar visible_sidebar' : 'sidebar'}>
+                <div onClick={toggleVisibleSidebar} className="sidebar_close">X</div>
+                <div className="sidebar__photo">
+                    <img src={iAm} alt="Alex Berezov foto" />
+                </div>
+                <div className="sidebar__navMenu">
+                    <ul>
+                        <li className="active">
+                            <NavLink to="/">Главная</NavLink>
+                        </li>
+                        <li>
+                            <NavLink to="/about">Обо мне</NavLink>
+                        </li>
+                        <li>
+                            <NavLink to="/resume">Резюме</NavLink>
+                        </li>
+                        <li>
+                            <NavLink to="/portfolio">Портфолио</NavLink>
+                        </li>
+                        <li>
+                            <NavLink to="/contacts">Контакты</NavLink>
+                        </li>
+                    </ul>
+                </div>
+                <div className="sidebar__copyright">
+                    <p>© 2021 <b>Алексей Березов</b></p>
+                </div>
             </div>
-            <div className="sidebar__copyright">
-                <p>© 2021 <b>Алексей Березов</b></p>
-            </div>
-        </div>
+        </>
     );
 }
 
